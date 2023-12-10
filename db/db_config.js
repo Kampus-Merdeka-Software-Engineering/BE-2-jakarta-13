dfdaf19bfcd72b758480ae13f9b97a71fb222070
itasaharani <itasaharani17@gmail.com>
2023-12-10 09:35:11 +0700

    add db port

--- db/db_config.js
+++ db/db_config.js
@@ -4,7 +4,8 @@ const conn = db.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'seren_hotel'
+  database: process.env.DB_NAME || 'seren_hotel',
+  port: process.env.DB_PORT || '3306'
 });
 
 conn.connect((err) => {
